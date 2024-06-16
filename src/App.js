cbefa29169793d7c5cf23b1a22c8189c3939b584
mhiztega <oghenetegafavour39@gmail.com>
2024-06-16 22:11:35 +0100

    edited

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a103b6b6`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=a103b6b6`;
     
     const response = await fetch(url);
     const responseJson = await response.json();
